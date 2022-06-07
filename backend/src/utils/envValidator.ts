e0782f38ffcc2320c175431c337b6e5b64bb0eda
cyber <cybericecream99@gmail.com>
2022-06-07 18:18:20 +0800

    Added basic Env Validators

---
+++ backend/src/utils/envValidator.ts
@@ -0,0 +1,81 @@
+export interface DbEnvVariables {
+    dbHost: string;
+    dbUser: string;
+    dbPass: string;
+    dbDatabase: string;
+    dbPort: number;
+    dbMaxConnections: number;
+}
+
+export interface ServerEnvVariables {
+    projectPort: number;
+}
+
+export interface JwtEnvVariables {
+    jwtSalt: string;
+}
+
+export class DbEnvValidator implements DbEnvVariables {
+    public readonly dbHost: string;
+    public readonly dbUser: string;
+    public readonly dbPass: string;
+    public readonly dbDatabase: string;
+    public readonly dbPort: number;
+    public readonly dbMaxConnections: number;
+
+    constructor() {
+        this.dbHost = validateEnvString('dbHost');
+        this.dbUser = validateEnvString('dbUser');
+        this.dbPass = validateEnvString('dbPass');
+        this.dbDatabase = validateEnvString('dbDatabase');
+        this.dbPort = validateEnvNumber('dbPort');
+        this.dbMaxConnections = validateEnvNumber('dbMaxConnections');
+    }
+}
+
+export class ServerEnvValidator implements ServerEnvVariables {
+    public readonly projectPort: number;
+
+    constructor() {
+        this.projectPort = validateEnvNumber('projectPort');
+    }
+}
+
+export class JetEnvValidator implements JwtEnvVariables {
+    public readonly jwtSalt: string;
+
+    constructor() {
+        this.jwtSalt = validateEnvString('salt');
+    }
+
+}
+
+const validateEnvString = (envVariable: string): string => {
+    try {
+        const envKey = process.env[`${envVariable}`];
+        if (!(envKey.length > 0)) {
+            throw new Error(`Couldn't Validate String ${envVariable}.`)
+        }
+        return envKey;
+    } catch (err) {
+        console.error({
+            message: err.message,
+            error: err
+        });
+    }
+}
+
+const validateEnvNumber = (envVariable: string): number => {
+    try {
+        const env = parseInt(process.env[`${envVariable}`]);
+        if (isNaN(env)) {
+            throw new Error(`Couldn't Validate String ${envVariable}.`)
+        }
+        return env;
+    } catch (err) {
+        console.error({
+            message: err.message,
+            error: err
+        });
+    }
+}

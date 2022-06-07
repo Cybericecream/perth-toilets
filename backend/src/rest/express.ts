f1f8b0084635a7c33a30a38f3e0816f9bf4d953c
cyber <cybericecream99@gmail.com>
2022-06-07 22:33:54 +0800

    Express server

--- backend/src/rest/express.ts
+++ backend/src/rest/express.ts
@@ -1,13 +1,14 @@
 import express, { Application, Request, Response } from "express";
 import {authRoutes} from "./auth/auth-routes";
 import {ServerEnvVariables} from "../utils/envValidator";
+import {AuthUserPasswordCommandHandler} from "../domain/auth-command-handler";
 
-export const server = async (serverEnv: ServerEnvVariables) => {
+export const server = async (serverEnv: ServerEnvVariables, authUserPasswordCommandHandler: AuthUserPasswordCommandHandler) => {
     const app: Application = express();
 
     app.use(express.json());
 
-    app.use("/auth", await authRoutes())
+    app.use("/auth", await authRoutes(authUserPasswordCommandHandler))
 
     app.get("/", (req: Request, res: Response) => {
         console.log(req);

8bb82b9503a85b0ec6a93766a6ec4d600ad47129
cyber <cybericecream99@gmail.com>
2022-06-10 19:05:58 +0800

    Added Errors.

---
+++ backend/src/rest/middleware/error-handler.ts
@@ -0,0 +1,28 @@
+import {ErrorRequestHandler} from "express";
+
+export const errorHandler: ErrorRequestHandler = async (err, req, res, next) => {
+    console.error({
+        title: `A new ${err.name || 'Error'} has been Thrown`,
+        uri: req.path,
+        message: err.message,
+        error: err
+    })
+    if (res.headersSent) {
+        return next(err);
+    }
+
+    switch (err.name) {
+        case 'TokenExpiredError':
+            return res.status(401).json({
+                error: "Expired Session."
+            })
+        case 'NoSessionFound':
+            return res.status(401).json({
+                error: "Login."
+            })
+        default:
+            return res.status(500).json({
+                error: "Internal Server Error."
+            })
+    }
+}

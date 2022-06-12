d3ca216aefaa3fe56e643b0cab510c6db98b8ad9
cyber <cybericecream99@gmail.com>
2022-06-12 22:26:35 +0800

    Simplified App entrypoint.

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,22 @@
 import {server} from "./rest/express";
+import {DbEnvValidator, JetEnvValidator, ServerEnvValidator} from "./utils/envValidator";
+import {AuthUserPasswordCommandHandler} from "./domain/auth-command-handler";
 import {Ledger} from "./data/mariadb/pool";
+import {AuthRepositories} from "./data/mariadb/repositories/auth-repositories";
+import {Hash} from "./utils/hash";
+import {JwtVerify} from "./utils/jwt";
 
-const ledger = new Ledger();
-console.log(ledger.connection.totalCount);
+const dbEnvVariables = new DbEnvValidator();
+const serverEnvVariables = new ServerEnvValidator();
+const jwtEnvVariables = new JetEnvValidator();
 
-server();
+const hasher = new Hash();
+
+const ledger = new Ledger(dbEnvVariables);
+const authRepositories = new AuthRepositories(ledger, hasher);
+
+const jwt = new JwtVerify(jwtEnvVariables);
+
+const authUserPasswordCommandHandler = new AuthUserPasswordCommandHandler(authRepositories, authRepositories, hasher, jwt);
+
+server(serverEnvVariables, authUserPasswordCommandHandler);
